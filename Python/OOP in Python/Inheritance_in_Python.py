790c8e406f9270bdaea699f4cb567eb13c0c85ac
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-01 13:22:12 +0300

    Inheritance in Python by Corey Schafer

--- Python/OOP in Python/Inheritance_in_Python.py
+++ Python/OOP in Python/Inheritance_in_Python.py
@@ -32,6 +32,45 @@ class Emp:
             return False
         return True
 
+class BusinessAnalyst(Emp):
+    raise_amount = 1.10
+    def __init__(self,first,last,pay,prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang = prog_lang
+
+class Manager(Emp):
+    def __init__(self,first,last,pay,employees=None):
+        super().__init__(first,last,pay)
+        if employees == None:
+            self.employees= []
+        else:
+            self.employees = employees
+
+    def add_employee(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove(self,emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for e in self.employees:
+            print('-->',e.fullname())
+
+
 #Now we can create instances of the class Emp
-employee_1 = Emp('Sospeter','Wainaina',120000)
-employee_2 = Emp('Alice','Nungari',50000)
+Ba_1 = BusinessAnalyst('Sospeter','Wainaina',120000,'SQL')
+Ba_2 = BusinessAnalyst('Alice','Nungari',50000,'Python')
+# print(employee_1.email)
+# print(help(BusinessAnalyst))
+# print(Ba_1.pay)
+# Ba_1.apply_raise()
+# print(Ba_1.pay)
+# print(Ba_1.prog_lang)
+# print(Ba_2.prog_lang)
+
+mgr_1 = Manager('Everlyne','Sunguti',260000,[Ba_1])
+print(mgr_1.email)
+mgr_1.add_employee(Ba_2)
+print(mgr_1.print_emps())
