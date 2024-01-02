2d5d00c271376e494ca1e9ebbd575e7b02ad9988
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-02 06:04:57 +0300

    How Python works under the hood

--- Python/OOP in Python/How_Python_works.py
+++ Python/OOP in Python/How_Python_works.py
@@ -12,4 +12,35 @@ class Dog:
 
 #You notice when it is compiled, there is no error. It is only at runtime that you get the error.
 # When we create an instance of the class, the __init__ method is called and the error is raised.
-dog_1 = Dog('Fido')
+# dog_1 = Dog('Fido')
+
+def make_class(x):
+    class Dog:
+        def __init__(self,name):
+            self.name = name
+
+        def print_value(self):
+            print(x)
+    # we return the class Dog
+    return Dog
+
+# We can now create instances of the class Dog
+cls = make_class(10)
+print(cls)
+
+d = cls('Siba')
+print(d.name)
+d.print_value()
+
+def func(x):
+    if x == 1:
+        def rv():
+            print('X is equal to 1')
+    else:
+        def rv():
+            print('X is not equal to 1')
+    return rv
+new_func = func(2)
+new_func()
+
+# We can actually rewrite the above code using decorators as follows:
