19893588179c7a58a0a210149ce141054ea58471
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-03 06:56:11 +0300

    Using Generator as context managers in python

--- Python/OOP in Python/context_managers.py
+++ Python/OOP in Python/context_managers.py
@@ -11,3 +11,37 @@ finally:
 with open("sample.txt","a") as file:
     file.write("Hello World")
 
+class File:
+    def __init__(self,file,method):
+        self.file = open(file,method)
+
+    # def __enter__(self): # this is the enter method which is called when we enter the context e.g. when we use the with statement "with open("sample.txt","a") as file:"
+    def __enter__(self):
+        print("Enter")
+        return self.file
+
+    def __exit__(self,type,value,traceback):
+        print(f"{type},{value},{traceback}")
+        print("Exit")
+        self.file.close()
+        return True
+
+with File('./sample.txt','w') as c:
+    print("Middle")
+    c.write("Hello")
+    raise Exception()
+
+# creating context managers using generators
+
+from contextlib import contextmanager
+@contextmanager
+def file(filename,method):
+    print("Enter")
+    file = open(filename,method)
+    yield file
+    file.close
+    print("Exit")
+
+with file("test.txt","w") as f:
+    print("Middle")
+    f.write("Hello World")
