683ffa4231c9639a46bcfc53166518f665a611fb
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-03 06:26:31 +0300

    Generators in python

---
+++ Python/OOP in Python/context_managers.py
@@ -0,0 +1,12 @@
+# these two pieces of code are equivalent
+
+file  = open('sile.txt',"a")
+try:
+    file.write("Hello World")
+finally:
+    file.close()
+
+# This is equivalent to the following code which is a context manager. It is better because it automatically closes the file for us
+
+with open("sample.txt","a") as file:
+    file.write("Hello World")
