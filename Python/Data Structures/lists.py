581b5a84ccba8315aeab1e427d40696e7897e05f
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-04 05:45:50 +0300

    Lists Datastructure in Python

--- Python/Data Structures/lists.py
+++ Python/Data Structures/lists.py
@@ -1,3 +1,50 @@
 # Lists are mutable, ordered, and allow duplicate elements
 #they are a collection of items in a particular order
-mylist =
+mylist = ["banana", "cherry", "apple"]
+print(mylist)
+#we can create an empty list and later come to append items to it like so
+mylist2 = list()
+print(mylist2)
+#They accept items of different data types
+mylist3  =  [5,"apple",True,5.6]
+print(mylist3)
+#accessing elements in the list we use the index with 0 being the first element
+item = mylist[0]
+print(item)
+#accessing elements with a negative index starts from the end of the list
+item = mylist[-2] #this will print banana
+#accessing an index that is out of range will throw an error of index out of range
+#we can also loop through the list
+
+for i in mylist:
+    print(i)
+
+#we can also check if an item is in the list
+
+if "banana" in mylist:
+    print("yes")
+else:
+    print("no")
+
+print("banana" in mylist)
+
+#we can also check the length iof the list
+print(len(mylist))
+#add items to a list we use append method which ill add an item to the end of the list
+mylist.append("orange")
+print(mylist)
+
+# we cn also insert an item at a soecific index
+mylist.insert(2,"lemon")
+print(mylist)
+
+#we can also remove items from a list
+mylist.pop() #this will remove the last item in the list
+print(mylist)
+
+#we can also remove an item at a specific index
+mylist.remove("lemon")
+print(mylist)
+
+# we can clear all the elements in a list
+
