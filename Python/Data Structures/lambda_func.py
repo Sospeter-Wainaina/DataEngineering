d4d04b880304972ba55efd214d676e055aa57f0a
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-05 06:51:40 +0300

    Lambda function in Python

--- Python/Data Structures/lambda_func.py
+++ Python/Data Structures/lambda_func.py
@@ -11,3 +11,13 @@ dlist = [(1,2),(15,4),(9,6),(7,8)]
 dlist_sorted = sorted(dlist, key = lambda x:x[1])
 print(dlist)
 print(dlist_sorted)
+
+a = [1,2,3,4,5,6,7,8,9,10]
+print([x**2 for x in a])
+
+# the same can be achieved using map and lambda
+# map applies a function to all the items in an input list
+
+print(list(filter(lambda x:x%2==0,list(map(lambda x:x**2,a)))))
+
+print([x for x in a if x%2==0])
