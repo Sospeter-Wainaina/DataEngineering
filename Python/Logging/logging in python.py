9c1b0c46e960091a84af5e0c67d573944634519a
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-08 05:52:10 +0300

    Logging in Python

---
+++ Python/Logging/logging in python.py
@@ -0,0 +1,13 @@
+import logging
+
+# we can log 5 levels of messages
+# debug
+logging.debug("This is a debug message")
+# info
+logging.info("This in an nfo message")
+# warning
+logging.warning("This is a warning message")
+# error
+logging.error("This is an error message")
+# critical
+logging.critical("This is a critical message")
