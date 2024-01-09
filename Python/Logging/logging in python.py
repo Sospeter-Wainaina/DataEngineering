b2b4f4399c36b8212bf3209c63e769e5ba879c52
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-09 06:14:08 +0300

    creating a handler in python

--- Python/Logging/logging in python.py
+++ Python/Logging/logging in python.py
@@ -1,21 +1,8 @@
 import logging
 
-logger = logging.getLogger(__name__)
+import logging.config
 
-# create handler
-stream_h = logging.StreamHandler()
-file_h = logging.FileHandler('file.log')
+logging.config.fileConfig('logging.conf')
 
-# level and the format
-stream_h.setLevel(logging.WARNING)
-file_h.setLevel(logging.ERROR)
-
-formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-stream_h.setFormatter(formatter)
-file_h.setFormatter(formatter)
-
-logger.addHandler(stream_h)
-logger.addHandler(file_h)
-
-logger.warning('This is a warning')
-logger.error('This is an error')
+logger = logging.getLogger('smpleExample')
+logger.debug('This is a debug message')
