42d8cda97e1770530f995fe058bfb814490d5783
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-17 06:47:32 +0300

    Directory

---
+++ DataEngineering-zoomcamp-2024/docker_sql/pipeline.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+import sys
+
+print(sys.argv)
+
+# sys.argv[0] #this is the name of the script
+# sys.argv[1] #this is the first argument
+day = sys.argv[1]
+#Do cool stuff with pandas
+
+print(f"Job finished successfully for day {day}")
