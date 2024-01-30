a9c5c05acb3d92d19e2a3afb5aefff5cb5395cc6
sospeter-wainaina <sospetergathungu2000@gmail.com>
2024-01-30 06:54:27 +0300

    Problem Statement

---
+++ SQL/SQL-Solutions/twitter_histogram.sql
@@ -0,0 +1,5 @@
+--PROBLEM STATEMENT
+/*
+ Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
+ 
+ In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.*/
